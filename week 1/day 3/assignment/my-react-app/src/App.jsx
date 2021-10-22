cc213763701143474f74787ce83f94ec2b4f50d3
vishalpatil12014017 <87421912+vishalpatil12014017@users.noreply.github.com>
2021-10-22 23:07:49 +0530

    updated TODO

--- week 1/day 3/assignment/my-react-app/src/App.jsx
+++ week 1/day 3/assignment/my-react-app/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Card from './components/Card'
 import './components/Card.css'
-function App() {
 
+function App() {
   return (
     <>
-    <Card date="28/10/2021" url="https://cdn3.iconfinder.com/data/icons/glypho-social-and-other-logos/64/logo-amazon-512.png" heading="Amezon Gift" subheading="Pay" device="Moto" color="orange"></Card>
+     
+      <Card date="28/10/2021" url="https://cdn3.iconfinder.com/data/icons/glypho-social-and-other-logos/64/logo-amazon-512.png" heading="Amezon Gift" subheading="Pay" device="Moto" color="orange"></Card>
     <Card date="28 sap 2021" url="https://cdn3.iconfinder.com/data/icons/picons-social/57/56-apple-512.png" heading="Apple Gift" subheading="Payment" device="Iphone" color="silver"></Card>
     </>
 
