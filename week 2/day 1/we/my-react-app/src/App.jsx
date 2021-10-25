b7cedc96ec017f70403426599967483c858e01d2
vishalpatil12014017 <87421912+vishalpatil12014017@users.noreply.github.com>
2021-10-26 00:19:02 +0530

    todo with api req

---
+++ week 2/day 1/we/my-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+import TodoInput from './components/TodosInput';
+import "./components/Todo.css"
+// const [show, setShow] = useState(true)
+
+function App() {
+  return (
+    <>
+      {/* <div className="main">
+        <Counter start="10" end="15"></Counter>
+      </div> */}
+      <div className="todo">
+        <TodoInput></TodoInput>
+      </div>
+    </>
+
+  );
+}
+
+export default App;
+
---
+++ week 2/day 1/we/my-react-app/src/components/TodosInput.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+function TodoInput() {
+    const [text, setText] = useState("");
+    const [data, setData] = useState([]);
+    const [page, setPage] = useState(1);
+    const [loading, setLoading] = useState(true)
+    const handleToggle = (id) => {
+        const updatedTodoList = data.map((e) => {
+            if (e.id === id) {
+                e.status = !e.status
+            }
+            return e
+        });
+        setData(updatedTodoList)
+    };
+    const handleRemove = (id) => {
+        data.map((e) => {
+            if (e.id === id) {
+                if (e.status === true) {
+                    fetch(`http://localhost:3001/Todos/${id}`, {
+                        method: "Delete",
+                    }).then(handleAdd)
+                }
+            }
+            return e
+        });
+    };
+    useEffect(() => {
+        handleAdd()
+    }, [page])
+    const handleClick = () => {
+        fetch("http://localhost:3001/Todos", {
+            method: "POST",
+            body: JSON.stringify({
+                title: text,
+                status: false
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(handleAdd)
+
+
+    };
+    const handleAdd = () => {
+        fetch(`http://localhost:3001/Todos?_page=${page}&_limit=3`).then((d) => d.json()).then(setData).then(()=>{
+            setLoading(false)
+        })
+    };
+
+    return loading?<h1>Loading.....</h1>: (
+        <>
+            <input type="text" onChange={(e) => {
+                setText(e.target.value)
+            }} placeholder="Enter todo" />
+            <button onClick={handleClick} >+
+            </button>
+            {data.map((e) => (
+                <div className="form-check" style={e.status ? { backgroundColor: "#88fa78", textDecoration: "underline" } : { backgroundColor: "#fac278" }}>
+                    <label className="form-check-label" for="flexCheckDefault" style={e.status ? { textDecoration: "line-through" } : { textDecoration: "none" }}>
+                        {e.title}
+                    </label>
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+                        onClick={() => {
+                            handleToggle(e.id, e.title);
+                        }} />
+                    <button style={e.status ? { backgroundColor: "#88fa78" } : { backgroundColor: "#fac278" }} className="float-end" onClick={() => {
+                        handleRemove(e.id);
+                    }}>X</button>
+                </div>
+            ))
+            }
+            <div>
+                <button className="next"
+                    onClick={() => {
+                        setPage(0 <= (page - 1))
+                    }}>Priv</button>
+                <button className="next"
+                    onClick={() => {
+                        setPage((page + 1))
+                    }}>Next</button>
+            </div>
+        </>
+    );
+}
+
+export default TodoInput;
