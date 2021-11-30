a58d19cd5acbd99d2f15d830e4347fd2d156cdcb
vishalpatil12014017 <87421912+vishalpatil12014017@users.noreply.github.com>
2021-11-30 23:03:21 +0530

    UseFetch

--- week 5/day 1/assignment/UseFetch/src/App.jsx
+++ week 5/day 1/assignment/UseFetch/src/App.jsx
@@ -1,9 +1,6 @@
 
 import './App.css';
-import Delay from './components/CustomHooks';
-import Throttle_debounce from './components/Throttle_debounce';
 import Fetch from './components/Fetch';
-import Reducer from './components/Reducer';
 function App() {
   return (
     <div className="App text-center text-white">
