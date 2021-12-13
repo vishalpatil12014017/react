0b8e21f1cc82ad3aa131c8964435d0144ad5ba2b
vishalpatil12014017 <87421912+vishalpatil12014017@users.noreply.github.com>
2021-12-13 16:52:44 +0530

    Done

--- Evaluation/c4/Cliant/src/Component/Form.jsx
+++ Evaluation/c4/Cliant/src/Component/Form.jsx
@@ -4,9 +4,16 @@ import useFetch from './UseFetch';
 import { useState, useEffect } from 'react';
 function useNotification() {
     const successNotification = (data, delay) => {
-
+        const timer = setTimeout(() => {
+            alert(data)
+        }, delay * 1000);
+        return () => clearTimeout(timer)
     }
     const failureNotification = (data, delay) => {
+        const timer1 = setTimeout(() => {
+            alert(data)
+        }, delay * 1000);
+        return () => clearTimeout(timer1)
 
     }
     return {
@@ -16,7 +23,6 @@ function useNotification() {
 }
 function Form() {
     const { loading, error, data } = useFetch("http://localhost:3001/User")
-    console.log('data:', data)
     const [formdata, setFormdata] = useState({})
     const handleChange = (e) => {
         const { name, value, type, checked } = e.target;
@@ -25,85 +31,71 @@ function Form() {
             [name]: type === "checkbox" ? checked : value,
         })
     };
-    const { successNotification, failureNotification, } = useNotification();
+    const { successNotification, failureNotification } = useNotification();
     const [suc, setsuc] = useState(false)
     const [err, seterr] = useState(false)
-    useEffect(() => {
-        var timer = setTimeout(() => {
-             setsuc(false)
-        }, 2000);
-        clearTimeout(timer)
-      
-        if (suc == true) {
-            return <div class="alert alert-primary" role="alert">
-                A simple primary alert—check it out!
-            </div>
-        }
-    }, [])
+    // useEffect(() => {
+    //     var timer = setTimeout(() => {
+    //          setsuc(false)
+    //     }, 2000);
+    //     clearTimeout(timer)
+
+    //     if (suc == true) {
+    //         return <div class="alert alert-primary" role="alert">
+    //             A simple primary alert—check it out!
+    //         </div>
+    //     }
+    // }, [])
 
     return (
-       loading?<h1 className='text-center text-white'>loading</h1>: <div className="p-5 border border-dark m-5 mx-auto bg-white" style={{ maxWidth: "500px", margin: "auto", borderRadius: "15px" }}>
-       <h1 className="text-center">Register Here</h1>
-       <form >
-           <div class="input-group mb-3">
-               <input type="text" class="form-control" placeholder="First Name" aria-label="Username" aria-describedby="basic-addon1" name="First_Name" onChange={handleChange} />
-           </div>
-           <div class="input-group mb-3">
-               <input type="text" class="form-control" placeholder="Last_Name" aria-label="Username" aria-describedby="basic-addon1" name="Last_Name" onChange={handleChange} />
-           </div>
-           <div className="mb-3">
-               <label for="exampleInputEmail1" className="form-label">Email address</label>
-               <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" onChange={handleChange} />
-               <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-           </div>
-           <div className="mb-3">
-               <label for="exampleInputPassword1" className="form-label">Date Of Birth</label>
-               <input type="date" className="form-control" name="date" onChange={handleChange} />
-           </div>
-           <div className="mb-3">
-               <label className="form-label">Mobile</label>
-               <input type="number" className="form-control" name="mobile" onChange={handleChange} />
-           </div>
-           <button type="submit" className="btn btn-primary mt-2" onClick={(e) => {
-               e.preventDefault();
+        <div className="p-5 border border-dark m-5 mx-auto bg-white" style={{ maxWidth: "500px", margin: "auto", borderRadius: "15px" }}>
+            <h1 className="text-center">Register Here</h1>
+            <form >
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="First Name" aria-label="Username" aria-describedby="basic-addon1" name="First_Name" onChange={handleChange} />
+                </div>
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Last_Name" aria-label="Username" aria-describedby="basic-addon1" name="Last_Name" onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" onChange={handleChange} />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Date Of Birth</label>
+                    <input type="date" className="form-control" name="date" onChange={handleChange} />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Mobile</label>
+                    <input type="number" className="form-control" name="mobile" onChange={handleChange} />
+                </div>
+                <button type="submit" className="btn btn-primary mt-2" onClick={(e) => {
+                    e.preventDefault();
+                    var len = data.filter((x) =>
+                        x.email == formdata.email
+                    )
+                    if (len.length === 0) {
+                        axios.post('http://localhost:3001/User', {
+                            email: formdata.email,
+                            password: formdata.password,
+                            First_Name: formdata.First_Name,
+                            Last_Name: formdata.Last_Name,
+                            mobile: formdata.mobile,
+                            DOB: formdata.date
+                        })
+                        successNotification("User Added Successfully", 2)
 
-               data.filter((x) => {
-                   if (x.email == formdata.email) {
-                       failureNotification("unsuccessful", 2)
-                       alert("Already Added")
-                   } else {
-                       axios.post('http://localhost:3001/User', {
-                           email: formdata.email,
-                           password: formdata.password,
-                           First_Name: formdata.First_Name,
-                           Last_Name: formdata.Last_Name,
-                           mobile: formdata.mobile,
-                           DOB: formdata.date
-                       })
-                       successNotification("ABC", 2)
-                       alert("User Successfully Added")
-                   }
-               })
-               if (data.length === 0) {
-                   axios.post('http://localhost:3001/User', {
-                       email: formdata.email,
-                       password: formdata.password,
-                       First_Name: formdata.First_Name,
-                       Last_Name: formdata.Last_Name,
-                       mobile: formdata.mobile,
-                       DOB: formdata.date
-                   })
-                   successNotification("QQQQ", 2000)
-                   alert("User Successfully Added")
-               }
+                    } else {
+                        failureNotification("unsuccessful", 2)
+                    }
 
 
+                }
 
-           }
-
-           }>Submit</button>
-       </form>
-   </div>
+                }>Submit</button>
+            </form>
+        </div>
     )
 }
 
--- Evaluation/c4/Testingc4/__tests__/storage.js
+++ Evaluation/c4/Testingc4/__tests__/storage.js
@@ -11,15 +11,27 @@ describe("Memory Converter", function () {
     test("Below 10 kb", () => {
         expect(storage(10000)).toBe("9.766 Kb");
     })
+    test("More Then 10 kb Less Than 100 Kb", () => {
+        expect(storage(100000)).toBe("97.656 Kb");
+    })
 
     test("More Then 100 Kb", () => {
         expect(storage(1000000)).toBe("976.563 Kb");
     })
+    test("More Then 1 Mb", () => {
+        expect(storage(10000000)).toBe("9.537 Mb");
+    })
 
-    test("Mb", () => {
+    test("More Then 10 Mb", () => {
         expect(storage(100000000)).toBe("95.367 Mb");
     })
+    test("More Then 100 Mb", () => {
+        expect(storage(1000000000)).toBe("953.674 Mb");
+    })
     test("Gb", () => {
+        expect(storage(1000000000000)).toBe("931.323 Gb");
+    })
+    test("1 Gb", () => {
         expect(storage(1000000000000)).toBe("931.323 Gb");
     })
 })
